05d19c0042b69a21ac1b3dc89e62df6f6d47f281
Leonardo Tiago <pequenotiago2016@gmail.com>
2024-07-03 07:56:51 +0100

    Inicialização do projecto agrisemu

---
+++ src/main/java/com/semulea/agrisemu/AgrisemuApplication.java
@@ -0,0 +1,13 @@
+package com.semulea.agrisemu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrisemuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrisemuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semulea/agrisemu/AgrisemuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semulea.agrisemu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrisemuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
