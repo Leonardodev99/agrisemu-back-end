54d0bf2805fc6154e44ea34cad43bdee6e0db3b0
Leonardo Tiago <pequenotiago2016@gmail.com>
2024-07-09 07:19:25 +0100

    User entity and resource

---
+++ src/main/java/com/semulea/agrisemu/entties/User.java
@@ -0,0 +1,76 @@
+package com.semulea.agrisemu.entties;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String username;
+	private String email;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String username, String email, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
