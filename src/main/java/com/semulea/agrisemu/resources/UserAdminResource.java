1b82922f60fc092d4ccbc9ad79fc20f77617d0f6
Leonardo Tiago <pequenotiago2016@gmail.com>
2024-07-16 14:15:58 +0100

    Impprovement in the implementation of the UserAdmin insert

--- src/main/java/com/semulea/agrisemu/resources/UserAdminResource.java
+++ src/main/java/com/semulea/agrisemu/resources/UserAdminResource.java
@@ -1,5 +1,6 @@
 package com.semulea.agrisemu.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.semulea.agrisemu.dto.UserAdminDTO;
 import com.semulea.agrisemu.services.UserAdminService;
@@ -37,7 +39,9 @@ public class UserAdminResource {
 	@PostMapping
 	public ResponseEntity<UserAdminDTO> insert(@RequestBody UserAdminDTO obj) {
 		obj = adminService.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 	
